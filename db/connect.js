07d9a0be9a86342ec27adef4e8f81d8a827450df
Nupur513 <128990611+Nupur513@users.noreply.github.com>
2024-06-27 20:03:58 +0530

    env changes

--- db/connect.js
+++ db/connect.js
@@ -1,18 +1,23 @@
+// Require dotenv and configure it to load variables from .env file
+require('dotenv').config();
+
 const mongoose = require("mongoose");
 
-const connectDB=(url)=>{
+const connectDB = () => {
+  const url = process.env.MONGODB_URI; // Get the MongoDB URI from environment variables
+
   return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
-  .then(()=>{
-      console.log('Connected to DB')
+  .then(() => {
+    console.log('Connected to DB');
   })
-  .catch((err)=>{
-      console.log(err)
-  })
-}
+  .catch((err) => {
+    console.error('Error connecting to DB:', err);
+  });
+};
 
-module.exports=connectDB
+module.exports = connectDB;
